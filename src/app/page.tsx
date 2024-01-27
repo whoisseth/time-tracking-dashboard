6546d974d49651d4caf79b2ff77ec42f18a8dacd
whoisseth <utkarshseth2019@gmail.com>
2024-01-28 01:07:08 +0530

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,173 @@
+/** @format */
+
+"use client";
+
 import Image from "next/image";
 
+import { BsThreeDots } from "react-icons/bs";
+
+import workImg from "@/assets/images/icon-work.svg";
+import userImage from "@/assets/images/image-jeremy.png";
+import playImage from "@/assets/images/icon-play.svg";
+import studyImage from "@/assets/images/icon-study.svg";
+import exerciseImage from "@/assets/images/icon-exercise.svg";
+import socialImage from "@/assets/images/icon-social.svg";
+import selfCareImage from "@/assets/images/icon-self-care.svg";
+
+import data from "@/assets/data.json";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import clsx from "clsx";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [timeFrame, setTimeFrame] = useState<"daily" | "weekly" | "monthly">(
+    "weekly"
+  );
+
+  const styleList = [
+    {
+      color: "bg-Work",
+      icon: workImg
+    },
+    {
+      color: "bg-Play",
+      icon: playImage
+    },
+    {
+      color: "bg-Study",
+      icon: studyImage
+    },
+    {
+      color: "bg-Exercise",
+      icon: exerciseImage
+    },
+    {
+      color: "bg-Social",
+      icon: socialImage
+    },
+    {
+      color: "bg-SelfCare",
+      icon: selfCareImage
+    }
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div className="min-h-screen w-full bg-VeryDarkBlue text-white flex flex-col items-center justify-center py-10 px-3">
+      <main className=" max-w-[850px] grid grid-cols-1 lg:grid-cols-4 gap-5 transition-all ">
+        <div className=" bg-DarkBlue row-span-2  rounded-xl overflow-hidden flex items-start flex-col">
+          {/* Blue div */}
+          <div className="bg-Blue rounded-xl px-6 pt-10 pb-16   flex-row lg:flex-col flex  gap-8 w-full">
+            {/* user Image */}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={userImage}
+              alt="user-image"
+              className="ring-2 h-12 w-12 rounded-full ring-white border-white "
             />
-          </a>
+
+            <section className=" flex-col flex gap-1">
+              <p className="text-PaleBlue text-xs">Report for</p>
+              <h2 className=" text-2xl lg:text-3xl font-thin">Jeremy Robson</h2>
+            </section>
+          </div>
+          <div className="flex lg:flex-col gap-4 justify-between px-6 \  py-4 text-PaleBlue text-sm font-thin items-start flex-row w-full">
+            <button
+              className={timeFrame === "daily" ? "text-white" : ""}
+              onClick={() => setTimeFrame("daily")}
+            >
+              Daily
+            </button>
+            <button
+              className={timeFrame === "weekly" ? "text-white" : ""}
+              onClick={() => setTimeFrame("weekly")}
+            >
+              Weekly
+            </button>
+            <button
+              className={timeFrame === "monthly" ? "text-white" : ""}
+              onClick={() => setTimeFrame("monthly")}
+            >
+              Monthly
+            </button>
+          </div>
+
+          {/*  */}
         </div>
+        {data.map((d, i) => {
+          let current;
+          let previous;
+          if (timeFrame === "daily") {
+            current = d.timeframes.daily.current;
+            previous = d.timeframes.daily.previous;
+          }
+          if (timeFrame === "monthly") {
+            current = d.timeframes.monthly.current;
+            previous = d.timeframes.monthly.previous;
+          }
+          if (timeFrame === "weekly") {
+            current = d.timeframes.weekly.current;
+            previous = d.timeframes.weekly.previous;
+          }
+
+          return (
+            <>
+              <TimeCard
+                color={styleList[i].color}
+                icon={styleList[i].icon}
+                key={i}
+                title={d.title}
+                current={current ?? 0}
+                previous={previous ?? 0}
+              />
+            </>
+          );
+        })}
+      </main>
+    </div>
+  );
+}
+
+type TimeCardType = {
+  title: string;
+  current: number;
+  previous: number;
+  color: string;
+  icon: string | StaticImport;
+};
+
+function TimeCard({
+  current = 32,
+  previous = 36,
+  title,
+  color,
+  icon
+}: TimeCardType) {
+  return (
+    <div
+      className={clsx(
+        " bg-orange-400  pt-10 rounded-xl overflow-hidden relative",
+        color
+      )}
+    >
+      <Image
+        className="  absolute top-[-10px] right-3 z-10  h-[70px] w-auto "
+        src={icon}
+        alt="time-card-img"
+      />
+      {/* blue */}
+      <div className="bg-DarkBlue relative z-20 rounded-t-lg p-6 flex-col flex gap-6">
+        {/* work  */}
+        <div className=" flex justify-between  w-full">
+          <p>{title}</p>
+          <BsThreeDots className="text-2xl" />
+        </div>
+
+        <section className=" lg:flex-col items-center justify-between flex gap-1 flex-row ">
+          <h2 className=" text-3xl lg:text-4xl font-thin"> {current}hrs </h2>
+
+          <p className=" text-PaleBlue text-xs"> Last Week - {previous}hrs </p>
+        </section>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
+/** @format */
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        Blue: "hsl(246, 80%, 60%)",
+        Work: "hsl(15, 100%, 70%)",
+        Play: "hsl(195, 74%, 62%)",
+        Study: "hsl(348, 100%, 68%)",
+        Exercise: "hsl(145, 58%, 55%)",
+        Social: "hsl(264, 64%, 52%)",
+        SelfCare: "hsl(43, 84%, 65%)",
+        VeryDarkBlue: "hsl(226, 43%, 10%)",
+        DarkBlue: "hsl(235, 46%, 20%)",
+        DesaturatedBlue: "hsl(235, 45%, 61%)",
+        PaleBlue: "hsl(236, 100%, 87%)"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
